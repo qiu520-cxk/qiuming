1c9ee625c51c7533ee354f0b4b39604fb74d73cd
a206 <a206@10.70.22.34>
2019-09-19 14:55:29 +0800

    提交代码

---
+++ src/java/K1.java
@@ -0,0 +1,11 @@
+package java;
+public class K1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int j=1;
+		 for( int i=5; i>0; i-=2 ) {
+		       j*=i;
+		 }
+		 System.out.print(j);
+		 }
+}
